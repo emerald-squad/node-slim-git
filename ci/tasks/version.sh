5b6ab3792a73a80bc98a433308040eba8e901e4e
Stephane Plante <stephane.a.plante@desjardins.com>
2018-12-17 10:10:17 -0500

    Fix the version scripts

--- ci/tasks/version.sh
+++ ci/tasks/version.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+VERSION_PREFIX=$1
+
 VERSION=$(git describe --first-parent --match '[[:digit:]]*.[[:digit:]]*')
 if [ $? -ne 0 ]
 then
     VERSION=$(git rev-parse --short HEAD)
 fi
 
-echo $VERSION
+echo $VERSION_PREFIX-$VERSION
